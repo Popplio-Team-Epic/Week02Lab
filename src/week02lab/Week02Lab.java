e77629fcb8e6f9f892eacaf2d264fdb4d4602154
Andrea <Andrea@Ezekiels-Laptop-Mark-Two.cg.shawcable.net>
2023-01-22 23:59:00 -0700

    Added text line Added simple System.out.println("Hello World!"); for debugging.

--- src/week02lab/Week02Lab.java
+++ src/week02lab/Week02Lab.java
@@ -15,7 +15,7 @@ public class Week02Lab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World of CPRG 352 by Ezekiel/Evie Villaraza!");
     }
     
 }
