46581c3be73a25df9eb42eece92a4a76aec7469b
Andrea <Andrea@Ezekiels-Laptop-Mark-Two.cg.shawcable.net>
2023-01-22 23:56:23 -0700

    New Project Creation Created a new Java Application for Week02Lab by Ezekiel/Evie Villaraza for CPRG 352 (Winter 2023).

---
+++ src/week02lab/Week02Lab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02lab;
+
+/**
+ *
+ * @author Andrea
+ */
+public class Week02Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
